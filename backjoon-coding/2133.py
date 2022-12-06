53b972f49f0cdf356226f46c4155da0db912f70d
kkwon <soonbin1997@gmail.com>
2022-12-06 21:17:09 +0900

    add file

---
+++ backjoon-coding/2133.py
@@ -0,0 +1,12 @@
+import sys
+num =  int(sys.stdin.readline())
+
+arr = [0]*(num+1)
+arr[0] = 1
+
+for i in range(2, num+1, 2):
+    arr[i] = arr[i-2] * 3
+    for j in range(0, i-2, 2):
+        arr[i] += arr[j] * 2
+
+print(arr[num])
