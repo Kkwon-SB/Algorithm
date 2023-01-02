cb80b7322b10f974708addbe19933595e75a2772
Soon Bin Kwon <76522430+Kkwon-SB@users.noreply.github.com>
2023-01-02 22:29:06 +0900

    평균 구하기

---
+++ backjoon-coding/1546.py
@@ -0,0 +1,20 @@
+subjects = int(input())
+
+scores = list(map(int, input().split()))
+
+# max_num = max(scores) 반복문이 길어질 경우 따로 값을 저장해서 사용하는 것이 효율적인 측면에서 좋을 것 같다.
+
+for i in range(len(scores)):
+    scores[i] = scores[i] / max(scores) * 100
+
+print(sum(scores)/subjects)
+
+'''
+에러 종류 - TypeError
+에러 메시지 - TypeError: ‘list’ object is not callable
+발생 이유 - 함수와 변수명이 중복되었을 때 발생
+python에서 쓰이는 함수를 변수명으로 선언한 뒤, 밑에서 그 함수를 호출하려고 하면 해당 에러가 발생한다.
+해결 방법 - 에러가 난 함수와 같은 이름의 변수가 있는지 확인
+'''
+
+
