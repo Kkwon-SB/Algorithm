3a027429eeb4fb7c5d944de7d5c1f605ebf5bdf3
Soon Bin Kwon <76522430+Kkwon-SB@users.noreply.github.com>
2023-04-26 04:51:42 +0900

    주사위 게임 2

---
+++ Programmers-coding/181930.py
@@ -0,0 +1,13 @@
+#https://school.programmers.co.kr/learn/courses/30/lessons/181930
+
+def solution(a, b, c):
+    answer = 0
+    
+    if a == b and b == c: #3개 같은 경우
+        answer = (a + b + c) * ((a*a) + (b*b) + (c*c)) * ((a*a*a) + (b*b*b) + (c*c*c)) 
+    elif a == b or b == c or a== c: #2개 같은 경우
+        answer = (a + b + c) * ((a*a) + (b*b) + (c*c)) 
+    else: #각자 다른 경우
+        answer = a + b + c
+    
+    return answer
