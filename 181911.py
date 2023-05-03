cae5c3732e79c5ddc59015d323f8ea760fe91d77
Soon Bin Kwon <76522430+Kkwon-SB@users.noreply.github.com>
2023-05-03 22:51:46 +0900

    부분 문자열 이어 붙여 문자열 만들기

---
+++ 181911.py
@@ -0,0 +1,10 @@
+#https://school.programmers.co.kr/learn/courses/30/lessons/181911
+#부분 문자열 이어 붙여 문자열 만들기
+
+def solution(my_strings, parts):
+    answer = ''
+    
+    for word, part in zip(my_strings, parts):
+        answer += word[part[0]:part[1]+1]    
+    
+    return answer
