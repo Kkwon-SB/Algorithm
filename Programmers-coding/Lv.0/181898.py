f52cdb684e4b73847c9d545a68bb8b97f395450e
Soon Bin Kwon <76522430+Kkwon-SB@users.noreply.github.com>
2023-05-06 03:51:07 +0900

    가까운 1 찾기

---
+++ Programmers-coding/Lv.0/181898.py
@@ -0,0 +1,10 @@
+#https://school.programmers.co.kr/learn/courses/30/lessons/181898
+#가까운 1 찾기
+
+def solution(arr, idx):
+    
+    for i in range(idx,len(arr)):
+        if arr[i] == 1:
+            return i
+        
+    return -1
