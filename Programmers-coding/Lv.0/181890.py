de4ec6fd47776e9ab947675763978e9bd576318c
Soon Bin Kwon <76522430+Kkwon-SB@users.noreply.github.com>
2023-05-08 02:27:56 +0900

    왼쪽 오른쪽

---
+++ Programmers-coding/Lv.0/181890.py
@@ -0,0 +1,12 @@
+#https://school.programmers.co.kr/learn/courses/30/lessons/181890
+#왼쪽 오른쪽
+
+def solution(str_list):
+
+    for i in range(len(str_list)):
+        if str_list[i] == 'l':
+            return str_list[:i]
+        if str_list[i] == 'r':
+            return str_list[i+1:]
+    
+    return []
