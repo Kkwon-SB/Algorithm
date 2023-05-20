d185dc4bdbe382c6309461123aeb9ea1d5c9a7a8
Soon Bin Kwon <76522430+Kkwon-SB@users.noreply.github.com>
2023-05-20 22:05:05 +0900

    이차원 배열 대각선 순회하기

---
+++ Programmers-coding/Lv.0/181829.py
@@ -0,0 +1,12 @@
+#https://school.programmers.co.kr/learn/courses/30/lessons/181829
+#이차원 배열 대각선 순회하기
+
+def solution(board, k):
+    answer = 0
+    
+    for i in range(len(board)):
+        for j in range(len(board[0])):
+            if i + j <= k:
+                answer += board[i][j]
+            
+    return answer
