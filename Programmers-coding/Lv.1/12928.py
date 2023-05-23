b58fa011276db7a6d1ce85afc9059d8b4a757263
Soon Bin Kwon <76522430+Kkwon-SB@users.noreply.github.com>
2023-05-23 21:44:53 +0900

    약수의 합

---
+++ Programmers-coding/Lv.1/12928.py
@@ -0,0 +1,11 @@
+#https://school.programmers.co.kr/learn/courses/30/lessons/12928
+#약수의 합
+
+def solution(n):
+    answer = 0
+    
+    for i in range(1, n//2+1):
+        if n % i == 0:
+            answer+=i
+    
+    return answer+n
