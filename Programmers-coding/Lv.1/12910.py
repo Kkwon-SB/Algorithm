b2217390dedeb9f264b1a12465defa19e133ba3c
Soon Bin Kwon <76522430+Kkwon-SB@users.noreply.github.com>
2023-06-06 23:49:26 +0900

    나누어 떨어지는 숫자 배열

---
+++ Programmers-coding/Lv.1/12910.py
@@ -0,0 +1,10 @@
+#https://school.programmers.co.kr/learn/courses/30/lessons/12910
+#나누어 떨어지는 숫자 배열
+
+def solution(arr, divisor):
+    answer = []
+    
+    for i in arr:
+        if i % divisor == 0:
+            answer.append(i)
+    return sorted(answer) if answer else [-1]
