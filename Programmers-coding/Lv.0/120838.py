e561659c23a886dc979df74d354ed1abcc03dc0b
Soon Bin Kwon <76522430+Kkwon-SB@users.noreply.github.com>
2023-07-13 23:37:44 +0900

    모스부호 (1)

---
+++ Programmers-coding/Lv.0/120838.py
@@ -0,0 +1,15 @@
+#https://school.programmers.co.kr/learn/courses/30/lessons/120838
+#모스부호 (1)
+
+def solution(letter):
+    letter = letter.split()
+    
+    morse = { 
+    '.-':'a','-...':'b','-.-.':'c','-..':'d','.':'e','..-.':'f',
+    '--.':'g','....':'h','..':'i','.---':'j','-.-':'k','.-..':'l',
+    '--':'m','-.':'n','---':'o','.--.':'p','--.-':'q','.-.':'r',
+    '...':'s','-':'t','..-':'u','...-':'v','.--':'w','-..-':'x',
+    '-.--':'y','--..':'z'
+    }
+    
+    return ''.join(morse[i] for i in letter)
