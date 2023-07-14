2bbcfb96e734c26151ab77a9d9453987b6168221
Soon Bin Kwon <76522430+Kkwon-SB@users.noreply.github.com>
2023-07-15 02:36:26 +0900

    주사위의 개수

---
+++ Programmers-coding/Lv.0/120845.py
@@ -0,0 +1,10 @@
+#https://school.programmers.co.kr/learn/courses/30/lessons/120845
+#주사위의 개수
+
+def solution(box, n):
+    answer = 1
+    
+    for i in box:
+        answer *= i//n
+        
+    return answer
