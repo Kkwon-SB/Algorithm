99a8a7fbf0f96b5b436fdf09264f0ed250d5a2fa
Soon Bin Kwon <76522430+Kkwon-SB@users.noreply.github.com>
2023-07-16 20:53:44 +0900

    배열 원소의 길이

---
+++ Programmers-coding/Lv.0/120854.py
@@ -0,0 +1,9 @@
+#https://school.programmers.co.kr/learn/courses/30/lessons/120854
+#배열 원소의 길이
+
+def solution(strlist):
+    return [len(i) for i in strlist]
+
+'''
+answer = list(map(len, strlist))
+'''
