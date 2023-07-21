cc1f1fbc57cddaf428c6b6b7d723fbb0ccd6ce05
Soon Bin Kwon <76522430+Kkwon-SB@users.noreply.github.com>
2023-07-21 15:13:51 +0900

    잘라서 배열로 저장하기

---
+++ Programmers-coding/Lv.0/120913.py
@@ -0,0 +1,10 @@
+#https://school.programmers.co.kr/learn/courses/30/lessons/120913
+#잘라서 배열로 저장하기
+
+def solution(my_str, n):
+    answer = []
+    
+    for i in range(0, len(my_str), n):
+        answer.append(my_str[i:i+n])
+    
+    return answer
